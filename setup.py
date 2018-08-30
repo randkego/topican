d2a73a4499ea292f76b853fbd9250555ab542a46
ZellSmith2 <Richard Smith>
2018-08-30 14:15:51 +0100

    First commit of Richards/topican - with .gitignore (and old/ excluded in .git/info/exclude

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topican",
+    version="0.0.2",
+    author="Richard Smith",
+    author_email="randkego@gmail.com",
+    description="Topic analyser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/topican",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
